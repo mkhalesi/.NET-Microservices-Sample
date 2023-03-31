7feaee16f9d1d5aae3042c962a55f6cdff4139b0
Mohammad Khalesi <mr.mohammadkhalesi@gmail.com>
2023-03-31 04:33:05 +0330

    addign ApiGateway.Admin project to solution

---
+++ src/ApiGateways/ApiGateway.Admin/Program.cs
@@ -0,0 +1,39 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = builder.Configuration;
+IWebHostEnvironment webHostEnvironment = builder.Environment;
+builder.Configuration.SetBasePath(webHostEnvironment.ContentRootPath)
+    .AddJsonFile("ocelot.json")
+    .AddOcelot(webHostEnvironment)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
+
+app.Run();
