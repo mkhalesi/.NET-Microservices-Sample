1e25cfd5ff46eb3cb2ba6d1a27a3b5f18b25c9d2
Mohammad Khalesi <mr.mohammadkhalesi@gmail.com>
2023-04-25 23:07:53 +0330

    move WebFrontend project to Str azure in Gittttt

--- src/Web/Microservices.Web.Frontend/Microservices.Web.Frontend/Models/DTO/Basket/DiscountInBasketDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microservices.Web.Frontend.Models.DTO.Basket
-{
-    public class DiscountInBasketDTO
-    {
-        public int Amount { get; set; }
-        public string DiscountCode { get; set; }
-    }
-}
--- src/Web/Microservices.Web.Frontend/Microservices.Web.Frontend/Models/DTO/Product/ProductCategory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microservices.Web.Frontend.Models.DTO.Product
-{
-    public class ProductCategory
-    {
-        public string categoryId { get; set; }
-        public string category { get; set; }
-    }
-}
