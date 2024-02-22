ad3086f8e746ddbc0a5deab3c3d795d6bb7e7d4e
Mohammad Khalesi <mr.mohammadkhalesi@gmail.com>
2024-02-22 13:27:15 +0330

    adding OrderService Project to AppHost project

--- src/Aspire/Microservices.AppHost/Program.cs
+++ src/Aspire/Microservices.AppHost/Program.cs
@@ -3,18 +3,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("rediscache");
 var rabbitMq = builder.AddRabbitMQContainer("rabbitmq");
-//var sqlserver = builder.AddSqlServerContainer("").AddDatabase("");
 var mongo = builder.AddContainer("mongo", "mongo");
 
 builder.AddProject<Projects.OrderService>("orderservice")
     .WithReference(cache)
-    .WithReference(rabbitMq);
-
-//.WithEnvironment("DatabaseSettings__ConnectionString", "mongodb://orderdb")
-//.WithEnvironment("DatabaseSettings__DatabaseName", "MicroserviceOrderDB")
-//.WithEnvironment("MicroServiceAddress__Product__Uri", "http://microservices-product.api")
-//.WithEnvironment("Identity__Uri", "http://${EXTERNAL_DNS_NAME_OR_IP}:7018")
-//.WithEnvironment("RabbitMq__Uri", "amqp://guest:guest@rabbitmq:5672")
-//.WithReference(rabbitMq);
+    .WithReference(rabbitMq)
+    .WithEnvironment("DatabaseSettings__ConnectionString", "mongodb://localhost:27017")
+    .WithEnvironment("DatabaseSettings__DatabaseName", "MicroserviceOrderDB")
+    .WithEnvironment("RabbitMq__Uri", "amqp://guest:guest@rabbitmq:5672");
 
 builder.Build().Run();
